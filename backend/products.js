a77c2e8b1b869f171b74b6f05690f439d7244e4e
shakilla1 <shakilshifat65@gmail.com>
2023-01-11 19:23:10 +0600

    first commit

--- backend/products.js
+++ backend/products.js
@@ -6,7 +6,7 @@ const products = [
     desc: "6.1-inch display",
     price: 999,
     image:
-      "https://res.cloudinary.com/chaoocharles/image/upload/v1629289889/online-shop/iphone12pro_e09phn.jpg",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2R28U89dTiZfkOvAFn9831Vy8vOwDIrWO2w&usqp=CAU",
   },
   {
     id: 2,
@@ -15,7 +15,7 @@ const products = [
     desc: "5.4-inch mini display",
     price: 699,
     image:
-      "https://res.cloudinary.com/chaoocharles/image/upload/v1629289889/online-shop/iphone12_efhrcp.jpg",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjHsSIesUR-jkAV6CJciIxTOEoHlyqogu8xQ&usqp=CAU",
   },
   {
     id: 3,
@@ -24,7 +24,7 @@ const products = [
     desc: "6.5-inch display",
     price: 399,
     image:
-      "https://res.cloudinary.com/chaoocharles/image/upload/v1629289889/online-shop/galaxyS_dvjf5w.png",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT27uGqQ46jhq-cDGRFCgQnZ8NY9lM9kU-H-w&usqp=CAU",
   },
 ];
 
